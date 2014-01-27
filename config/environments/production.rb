1c9734d37badc8a0512d18710b5ad9273d0ad148
Rick Guetschow <rick.guetschow@gmail.com>
2014-01-26 18:00:48 -0800

    added amazon s3 for images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,12 @@ Pinteresting::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   config.action_mailer.default_url_options = { :host => 'rickgbass.com' }
+  #Sets paperclip to upload images to Amazon S3
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV[' AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+  }
 end
