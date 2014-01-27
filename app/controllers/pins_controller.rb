4c89af261e1ffac153f022aedb681837e3840962
Rick Guetschow <rick.guetschow@gmail.com>
2014-01-26 17:36:34 -0800

    added paperclip with image upload

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -67,7 +67,7 @@ class PinsController < ApplicationController
       redirect_to pins_path, notice: "not authorized to edit this pin" if @pin.nil?
     end  
     def pin_params
-      params.require(:pin).permit(:description)
+      params.require(:pin).permit(:description, :image)
     end
 
 end
